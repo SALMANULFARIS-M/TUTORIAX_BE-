daecdee2449b9f49075f5927bb1cf475119aea31
Salmanulfaris-M <fsalmanul13@gmail.com>
2023-06-20 10:35:00 +0530

    error handling

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ declare module 'express';
 import express, { Application, Request, Response, NextFunction } from "express";
 import studentRoutes from "./routes/student_routes";
 import adminRoutes from "./routes/admin_routes";
-import teacherRoutes  from "./routes/teacher_routes";
+import teacherRoutes from "./routes/teacher_routes";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -15,21 +15,36 @@ dotenv.config();
 const app: Application = express();
 
 app.use(
-    cors({
-      credentials: true,
-      origin: [process.env.ORIGIN as string],
-    })
-  );
-  app.use(express.static(path.join(__dirname, "public/images/")));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(cookieParser());
-  app.use(express.json());
-  app.use("/", studentRoutes);
-  app.use("/admin", adminRoutes);
-  app.use("/teacher",teacherRoutes)
+  cors({
+    credentials: true,
+    origin: [process.env.ORIGIN as string],
+  })
+);
+app.use(express.static(path.join(__dirname, "public/images/")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/", studentRoutes);
+app.use("/admin", adminRoutes);
+app.use("/teacher", teacherRoutes);
 
-  mongoose
+// Handle 404 errors
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ error: 'Not found' });
+});
+
+// Handle 500 errors
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+// Handle 502 errors
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(502).json({ error: 'Bad gateway' });
+});
+
+mongoose
   .connect(process.env.MONGODBSERVER as string)
   .then(() => {
     app.listen(process.env.PORT as string, () => {
